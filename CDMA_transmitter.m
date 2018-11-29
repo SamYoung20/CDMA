447942ee0b2f682628d27aa2021145ea77e46b47
SamYoung20 <samantha.young@students.olin.edu>
2018-11-29 11:20:08 -0500

    Add files via upload

---
+++ CDMA_transmitter.m
@@ -0,0 +1,27 @@
+function signal = transmit(num_users, user_data, code)
+ % if ~exist('type','var')
+     % third parameter does not exist, so default it to something
+  %    type = 'rcos';
+  end
+  samplesPerSymbol = round(samplerate/symbolrate);
+  
+  m = repmat(symbols,[samplesPerSymbol,1]);
+  signalLen = samplesPerSymbol*length(symbols);
+  symbolsResampled = reshape(m,[1, signalLen]);
+  
+  %raised cosine filter 
+  h = rcosdesign(0.25, length(symbols) ,samplesPerSymbol, 'normal');
+  shapedPulses = conv(symbolsResampled, h, 'same');  
+  
+  time = length(symbols)/symbolrate;
+  t = linspace(0,time,signalLen);
+  
+  carrier = exp(i*carrierFreq*2*pi*t);
+  
+  %change shapedPulses to symbolResampled to not use the RCF
+  if(strncmp(type,'rcos',4))
+    signal = carrier.*shapedPulses;
+  else
+    signal = carrier.*symbolsResampled;
+  end
+end
